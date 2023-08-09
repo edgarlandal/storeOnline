ba7dff24d787c71729a8e064d4026830f5e7d87b
edgarlandal <mlanda@uabc.edu.mx>
2023-08-09 11:44:51 -0700

    Last Change

--- client/src/pages/catalog.jsx
+++ client/src/pages/catalog.jsx
@@ -1,17 +1,21 @@
+import { useEffect } from "react";
 import Products from "../components/products";
 import { catalog } from "../services/dataServices";
 
 import "./catalog.css";
 
 function Catalog() {
-  catalog.map((i, product) => (
-    console.log(i)
-  ));
+
+  useEffect(() => {
+    
+  });
+
+
   return (
     <div className="catalog">
       <h1>Check out our amazing catalog</h1>
       {catalog.map((product, i) => (
-        <Products product={product} key={i}/>
+        <Products product={product} key={i} />
       ))}
     </div>
   );
