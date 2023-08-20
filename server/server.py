eaf6614c42366d0c8242094d41a75a9eff77e8c8
edgarlandal <mlanda@uabc.edu.mx>
2023-08-19 22:38:57 -0700

    Class 2

--- server/server.py
+++ server/server.py
@@ -1,13 +1,44 @@
 from flask import Flask
+from config import me
+import json
 
 app = Flask("server")
 
-@app .get("/")
+
+@app.get("/")
 def home():
     return "Hello world"
+
 
 @app.get("/test")
 def test():
     return "This is a test page"
 
-app.run(debug = True)
+
+@app.get("/about")
+def about_me():
+    return "Edgar Landa"
+
+
+"""API - Products """
+""" JSON"""
+
+
+@app.get("/api/about")
+def about_data():
+    return json.dumps(me)
+
+
+@app.get("/api/about/developer")
+def get_name():
+    return json.dumps(me["name"] + " " + me["last_name"])
+
+
+@app.get("/api/categories")
+def categories():
+    all_cats = ["fruits", "vegetables", "dairy & eggs"]
+    return json.dumps(all_cats)
+
+
+# start the server
+app.run(debug=True)
